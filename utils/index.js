f0cc1bb3bb12668912126eb0eeb68a4e4483e95d
Edward Edberg <edward.edberg.halim@gmail.com>
2022-04-22 15:26:44 -0700

    sucess send email verification

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,14 @@
-const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
-const createTokenUser = require('./createTokenUser');
-const checkPermissions = require('./checkPermissions');
+const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt");
+const createTokenUser = require("./createTokenUser");
+const checkPermissions = require("./checkPermissions");
+const sendEmail = require("./sendEmail");
+const sendVerificationEmail = require("./sendVerficationEmail");
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createTokenUser,
   checkPermissions,
+  sendEmail,
+  sendVerificationEmail,
 };
---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "cecilia.collier35@ethereal.email",
+    pass: "BGq8qKCJChFpdBfYVJ",
+  },
+};
